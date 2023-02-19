9917a21e39b47dabd12f66b868d28e4ebcec8c6b
Russell Heimlich <kingkool68@gmail.com>
2023-02-18 22:10:57 -0500

    Removing gulp config options that don't get used

--- gulp-config.js
+++ gulp-config.js
@@ -13,8 +13,6 @@ module.exports = {
 	project: 'Russell Heimlich', // Project Name.
 	projectURL: '', // Local project URL of your already running WordPress site. Could be something like wpgulp.local or localhost:8888.
 	productURL: './', // Theme/Plugin URL. Leave it like it is, since our gulpfile.js lives in the root folder.
-	browserAutoOpen: false,
-	injectChanges: true,
 
 	// Style options.
 	styleSRC: ['./assets/scss/**/*.scss', './blocks/**/*.scss'], // Path to main .scss file.
@@ -22,22 +20,15 @@ module.exports = {
 	outputStyle: 'compressed', // Available options → 'compact' or 'compressed' or 'nested' or 'expanded'
 	errLogToConsole: true,
 	precision: 10,
-	includePaths: ['./assets/scss'], // Tell Sass where to look for @imports
+	includePaths: ['./assets/scss'], // Tell Sass where to look for @import statements
 
 	// JS Custom options.
 	jsCustomSRC: './assets/js/**/*.src.js', // Path to JS custom scripts folder.
 	jsCustomDestination: './assets/js/', // Path to place the compiled JS custom scripts file.
-	jsCustomFile: 'custom', // Compiled JS custom file name. Default set to custom i.e. custom.js.
-
-	// Images options.
-	// imagesSRC: './assets/img/raw/**.{png,jpg,gif,svg}', // Source folder of images which should be optimized.
-	imgSRC: './assets/img/raw/*', // Source folder of images which should be optimized and watched.
-	imgDST: './assets/img/', // Destination folder of optimized images. Must be different from the imagesSRC folder.
 
 	// Watch files paths.
-	styleWatchFiles: ['./assets/scss/**/*.scss', './blocks/**/*.scss'], // Path to all *.scss files inside css folder and inside them.
-	customJSWatchFiles: './assets/js/**/*.src.js', // Path to all custom JS files.
-	projectPHPWatchFiles: './**/*.php', // Path to all PHP files.
+	styleWatchFiles: ['./assets/scss/**/*.scss', './blocks/**/*.scss'], // *.scss files to watch changes and recompile
+	customJSWatchFiles: ['./assets/js/**/*.src.js', './blocks/**/*.src.js'], // *.src.js files to watch changes and recompile
 
 	// Dependencies to move into place
 	// NOTE: rename is object of options to pass to gulp-rename
@@ -62,22 +53,10 @@ module.exports = {
 		*/
 	],
 
-	/*
-	// Translation options.
-	textDomain: '', // Your textdomain here.
-	translationFile: '', // Name of the transalation file.
-	translationDestination: './languages', // Where to save the translation files.
-	packageName: '', // Package name.
-	bugReport: '', // Where can users report bugs.
-	lastTranslator: '', // Last translator Email ID.
-	team: '', // Team's Email ID.
-	*/
 	// Browsers you care about for autoprefixing. Browserlist https://github.com/ai/browserslist
 	BROWSERS_LIST: [
 		'last 10 versions',
 		'ie >= 6',
 		'Android >= 2.3'
 	]
-
-	// STOP Editing Project Variables.
 };
