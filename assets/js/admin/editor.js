efe9649bb443e94530f98361acd3904ab39fee26
kingkool68 <kingkool68+github-actions[bot]@users.noreply.github.com>
2025-10-05 05:23:29 +0000

    Merged 982ac226ea2318cf9d82d1589c6cc6e04aa09a50 from main
    https://github.com/kingkool68/wordpress-rh-starter-theme/commit/982ac226ea2318cf9d82d1589c6cc6e04aa09a50/checks

--- assets/js/admin/editor.js
+++ assets/js/admin/editor.js
@@ -1,2 +1,2 @@
 (()=>{(function(a){typeof acf=="object"&&(acf.add_filter("select2_args",function(e,c,s,l,i){return e.templateSelection=function(t){var n=a('<span class="acf-selection"></span>');return n.html(t.text),n.data("element",t.element),n},e}),acf.add_filter("select2_escape_markup",function(e,c,s,l,i,t){return c}))})(jQuery);})();
-//# sourceMappingURL=editor.js.map
+//# sourceMappingURL=editor.src.js.map
