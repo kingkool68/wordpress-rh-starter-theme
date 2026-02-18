080f9c63af8974eb9666e3efee99d83fb2ce6450
Russell Heimlich <kingkool68@gmail.com>
2026-02-18 00:00:51 -0500

    Linting

--- single-post.php
+++ single-post.php
@@ -4,10 +4,10 @@ setup_postdata( get_post() );
 $date = RH_Helpers::get_date_values( $post->post_date );
 
 $context = array(
-	'the_title'            => get_the_title(),
-	'display_date'         => $date->display_date,
-	'machine_date'         => $date->machine_date,
-	'featured_image'       => RH_Media::render_image_from_post(
+	'the_title'      => get_the_title(),
+	'display_date'   => $date->display_date,
+	'machine_date'   => $date->machine_date,
+	'featured_image' => RH_Media::render_image_from_post(
 		$post->ID,
 		array(
 			'link_url'   => get_the_post_thumbnail_url( $post->ID, 'original' ),
@@ -16,6 +16,6 @@ $context = array(
 			),
 		)
 	),
-	'the_content'          => apply_filters( 'the_content', get_the_content() ),
+	'the_content'    => apply_filters( 'the_content', get_the_content() ),
 );
 Sprig::out( 'single-post.twig', $context );
